1c5cba331902fdd0f6b66445bc8e86f44539c2c2
Andrew Thow <thow1976@hotmail.co.uk>
2022-07-06 20:42:55 +0100

    6_325_Changing_Between_Login_Sign_Up

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,14 +1,24 @@
+import 'dart:collection';
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
   //Capturing form info to be passed to TextField onpressed function
   final _formKey = GlobalKey<FormState>();
-  String _userEmail = '';
-  String _userPassword = '';
-  String _userName = '';
+
+  //Controls whether login or sign up page is shown
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userPassword = '';
+  var _userName = '';
   late BuildContext context;
 
   //Method for implementing all the validators of the textform fields
@@ -54,17 +64,19 @@ class AuthForm extends StatelessWidget {
                         onSaved: (value) {
                           _userEmail = value!;
                         }),
-                    TextFormField(
-                        validator: (value) {
-                          if (value!.isEmpty || value.length < 4) {
-                            return 'Please enter at least 4 characters.';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(labelText: 'Username'),
-                        onSaved: (value) {
-                          _userName = value!;
-                        }),
+                    //Controls whether the username field is displayed according to login/sign up
+                    if (!_isLogin)
+                      TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty || value.length < 4) {
+                              return 'Please enter at least 4 characters.';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(labelText: 'Username'),
+                          onSaved: (value) {
+                            _userName = value!;
+                          }),
 
                     TextFormField(
                         validator: (value) {
@@ -84,13 +96,22 @@ class AuthForm extends StatelessWidget {
                       height: 12,
                     ),
                     RaisedButton(
-                      child: Text('Login'),
+                      //Setting up the login/sign up buttons to offer different states
+                      child: Text(_isLogin ? 'Login' : 'Sign Up'),
                       onPressed: _trySubmit,
                     ),
                     FlatButton(
                       textColor: Theme.of(context).primaryColor,
-                      onPressed: () {},
-                      child: Text('Create Account'),
+                      //Setting up the create account/already have account states
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        //Initialising the state change for the sign in options
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
                     ),
                   ],
                 ),
