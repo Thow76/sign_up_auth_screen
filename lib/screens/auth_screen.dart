c8d8ca89a91cbff6c2c451185130fb1d23cbce83
Andrew Thow <thow1976@hotmail.co.uk>
2022-07-06 22:52:18 +0100

    9_328_Implementing_Firebase_Auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import '../widgets/auth_form.dart';
@@ -9,11 +10,26 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  //Function that adds info to Firebase
+  //Implementing FIrebase Auth
+  final _auth = FirebaseAuth.instance;
 
+  //Function that adds info to Firebase
   void _submitAuthForm(
-      String email, String password, String userName, bool isLogin) {}
-  @override
+      String email, String password, String userName, bool isLogin)
+
+  //Implementing Firebase auth email sign in
+  async {
+    UserCredential authResult;
+
+    if (isLogin) {
+      authResult = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } else {
+      authResult = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
