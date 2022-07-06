d487963dee4052bd731b0abf69d244f78ff003a6
Andrew Thow <thow1976@hotmail.co.uk>
2022-07-06 20:13:45 +0100

    5_325_Setting_Up_Submit_Criteria

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class AuthForm extends StatelessWidget {
-  const AuthForm({Key? key}) : super(key: key);
+  //Capturing form info to be passed to TextField onpressed function
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userPassword = '';
+  String _userName = '';
+  late BuildContext context;
+
+  //Method for implementing all the validators of the textform fields
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+
+    //Closes keyboard after input form submitted
+    FocusScope.of(context).unfocus();
+
+    //Checking if form is valid and saving input via the onsaved function
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,47 +34,58 @@ class AuthForm extends StatelessWidget {
               padding: EdgeInsets.all(16),
               // Input Form
               child: Form(
+                //Connecting _formKey variable to form
+                key: _formKey,
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     //Validating input
 
                     TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || !value.contains('@')) {
-                          return 'Please enter a valid email address.';
-                        }
-                        return null;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(labelText: 'Email address'),
-                    ),
+                        validator: (value) {
+                          if (value!.isEmpty || !value.contains('@')) {
+                            return 'Please enter a valid email address.';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(labelText: 'Email address'),
+                        //Function saving the input
+                        onSaved: (value) {
+                          _userEmail = value!;
+                        }),
                     TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || value.length < 4) {
-                          return 'Please enter at least 4 characters.';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(labelText: 'Username'),
-                    ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 4) {
+                            return 'Please enter at least 4 characters.';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(labelText: 'Username'),
+                        onSaved: (value) {
+                          _userName = value!;
+                        }),
+
                     TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty || value.length < 7) {
-                          return 'Password must be at least 7 characters long.';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(labelText: 'Password'),
-                      // Hide password
-                      obscureText: true,
-                    ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 7) {
+                            return 'Password must be at least 7 characters long.';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(labelText: 'Password'),
+                        // Hide password
+                        obscureText: true,
+                        onSaved: (value) {
+                          _userPassword = value!;
+                        }),
+
                     SizedBox(
                       height: 12,
                     ),
                     RaisedButton(
                       child: Text('Login'),
-                      onPressed: () {},
+                      onPressed: _trySubmit,
                     ),
                     FlatButton(
                       textColor: Theme.of(context).primaryColor,
